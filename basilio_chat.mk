73fb68735144079574b3c80e78a2b7168231c40c
Charles Van West <charles@vanwestco.com>
2018-07-16 20:49:02 -0400

    Update missed files in the last commit.

--- basilio_chat.mk
+++ basilio_chat.mk
@@ -1,17 +1,15 @@
 OBJECTS = basilio_chat.o packet.o
 
 basilio_chat: main.c++ basilio_chat.o packet.o terminal/terminal.o ../socket/socket.o audio/core_audio.o
-	c++ -I.. -I/Users/Charlie/.cpath/portaudio \
-	-L/Users/Charlie/.cpath/portaudio/lib -lportaudio \
-	-L/usr/local/opt/argp-standalone/lib -largp \
+	c++ -lportaudio -lpthread \
 	-o basilio_chat main.c++ basilio_chat.o packet.o terminal/terminal.o \
 	../socket/socket.o audio/core_audio.o
 
 basilio_chat.o: basilio_chat.c++ basilio_chat.h++
-	c++ -I.. -I/Users/Charlie/.cpath/portaudio -c -o basilio_chat.o basilio_chat.c++
+	c++ -c -o basilio_chat.o basilio_chat.c++
 
 packet.o: packet.c++ packet.h++
-	c++ -I.. -I/Users/Charlie/.cpath/portaudio -c -o packet.o packet.c++
+	c++ -c -o packet.o packet.c++
 
 .PHONY: clean
 clean:
